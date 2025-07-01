1b99c3560bc3f829d717b57bee41725a1293db66
Leonardo Ferreira <leoalmeida.rj@gmail.com>
2025-07-01 20:32:58 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 const connect = (url) => {
 
     try {
-        //const url = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/${MONGO_DB}?retryWrites=true&w=majority&appName=ClusterLeo`
-        //const url = `mongodb+srv://api-nodejs:8VNergbNwt06HL7R@clusterleo.sckgivp.mongodb.net/db?retryWrites=true&w=majority&appName=ClusterLeo`
         mongoose.connect(url);
     } catch (error) {
         console.error("Mongoose connection or check failed:", error);
